d3cccee5157dc61e16ff2cfaa8151ffd1bf6ecb6
mdbeach <mikey.beach4@gmail.com>
2024-07-23 18:18:02 -0500

    WE ARE SO BACK

--- Cerklo/Cerklo.Game/CerkloGame.cs
+++ Cerklo/Cerklo.Game/CerkloGame.cs
@@ -11,10 +11,6 @@ namespace Cerklo.Game
         [BackgroundDependencyLoader]
         private void load()
         {
-            Resources.AddStore(new DllResourceStore(@"AwesomeGame.dll"));
-        }
-        private void load()
-        {
             // Add your top-level game components here.
             // A screen stack and sample screen has been provided for convenience, but you can replace it if you don't want to use screens.
             Child = screenStack = new ScreenStack { RelativeSizeAxes = Axes.Both };
